24af9c56044b81772ee80f2d5508931dd972407c
nilllzz <nidresch@googlemail.com>
2015-08-04 11:27:29 +0200

    Updated comments on IShape.cs

--- Pokémon3D/UI/IShape.cs
+++ Pokémon3D/UI/IShape.cs
@@ -2,44 +2,28 @@
 
 namespace Pokémon3D.UI
 {
-    /// <summary>
-    /// An interface for standard shapes.
-    /// </summary>
+    /// <summary>An interface for standard shapes.</summary>
     interface IShape
     {
-        /// <summary>
-        /// The outer bounds of this shape.
-        /// </summary>
+        /// <summary>The outer bounds of this shape.</summary>
         Rectangle Bounds { get; }
 
-        /// <summary>
-        /// The location of this shape.
-        /// </summary>
+        /// <summary>The location of this shape.</summary>
         Point Location { get; }
 
-        /// <summary>
-        /// Calculates the area of this shape.
-        /// </summary>
+        /// <summary>Calculates the area of this shape.</summary>
         double GetArea();
 
-        /// <summary>
-        /// Returns if this shape contains the x and y coodinates.
-        /// </summary>
+        /// <summary>Returns if this shape contains the x and y coodinates.</summary>
         bool Contains(int x, int y);
 
-        /// <summary>
-        /// Returns if this shape contains a <see cref="Vector2"/>.
-        /// </summary>
+        /// <summary>Returns if this shape contains a <see cref="Vector2"/>.</summary>
         bool Contains(Vector2 value);
 
-        /// <summary>
-        /// Returns if this shape contains a <see cref="Rectangle"/>.
-        /// </summary>
+        /// <summary>Returns if this shape contains a <see cref="Rectangle"/>.</summary>
         bool Contains(Rectangle value);
 
-        /// <summary>
-        /// Returns if this shape contains a <see cref="Point"/>.
-        /// </summary>
+        /// <summary>Returns if this shape contains a <see cref="Point"/>.</summary>
         bool Contains(Point value);
     }
 }
