a7a8e279e9f99936583f521824b1ed278d51e6aa
trommlbomml <peter_friedland@gmx.de>
2015-09-20 13:18:31 +0200

    Added classes for rendering framework.

---
+++ Pokémon3D/Rendering/Mesh.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Pokémon3D.Rendering
+{
+    class Mesh
+    {
+        private readonly VertexBuffer _vertexBuffer;
+        private readonly IndexBuffer _indexBuffer;
+
+        public Mesh(GraphicsDevice device, GeometryData data)
+        {
+            _vertexBuffer = new VertexBuffer(device, VertexPositionNormalTexture.VertexDeclaration, data.Vertices.Length, BufferUsage.WriteOnly);
+            _indexBuffer = new IndexBuffer(device, IndexElementSize.SixteenBits, data.Indices.Length, BufferUsage.WriteOnly);
+
+            _vertexBuffer.SetData(data.Vertices);
+            _indexBuffer.SetData(data.Indices);
+        }
+
+        public void Draw()
+        {
+            var device = _vertexBuffer.GraphicsDevice;
+            device.SetVertexBuffer(_vertexBuffer);
+            device.Indices = _indexBuffer;
+
+            var primitiveCount = _indexBuffer.IndexCount / 3;
+            device.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0,0,_vertexBuffer.VertexCount, 0, primitiveCount);
+        }
+    }
+}
---
+++ Pokémon3D/Rendering/Scene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pokémon3D.Rendering
+{
+    class Scene
+    {
+        private readonly List<SceneNode> _allNodes;
+
+        public Scene()
+        {
+            _allNodes = new List<SceneNode>();
+        }
+
+        public SceneNode CreateSceneNode()
+        {
+            var sceneNode = new SceneNode();
+            _allNodes.Add(sceneNode);
+            return sceneNode;
+        }
+
+        public void Update(float elapsedTime)
+        {
+            foreach (var sceneNode in _allNodes)
+            {
+                sceneNode.Update();
+            }
+        }
+
+        public void Draw(GraphicsDevice device)
+        {
+            foreach ( var sceneNode in _allNodes)
+            {
+                if (sceneNode.Mesh == null) continue;
+
+                sceneNode.Mesh.Draw();
+            }
+        }
+    }
+}
