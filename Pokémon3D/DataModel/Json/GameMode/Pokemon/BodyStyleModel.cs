c5f5112c9f500689268a4c7d24ab7f7808aec22b
nilllzz <nidresch@googlemail.com>
2015-09-22 18:12:59 +0200

    More warning disables on missed Data model files

--- Pokémon3D/DataModel/Json/GameMode/Pokemon/BodyStyleModel.cs
+++ Pokémon3D/DataModel/Json/GameMode/Pokemon/BodyStyleModel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using Microsoft.Xna.Framework;
 
+// Disable Code Analysis for warning CS0649: Field is never assigned to, and will always have its default value.
+#pragma warning disable 0649
+
 namespace Pokémon3D.DataModel.Json.GameMode.Pokemon
 {
     /// <summary>
