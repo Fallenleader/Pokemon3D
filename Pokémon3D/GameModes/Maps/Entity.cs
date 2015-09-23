88c6fc6ee40254deeced2c7904c02b1245d2363b
nilllzz <nidresch@googlemail.com>
2015-09-23 23:24:18 +0200

    Added Entity-class

---
+++ Pokémon3D/GameModes/Maps/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokémon3D.GameModes.Maps
+{
+    enum EntityRenderMode
+    {
+
+    }
+
+    enum EntityFaceDirection
+    {
+
+    }
+
+    class Entity
+    {
+    }
+}
