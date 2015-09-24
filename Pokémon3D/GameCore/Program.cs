bca5566849531b7949147a3c06fa28a3d8b16e3f
nilllzz <nidresch@googlemail.com>
2015-09-24 23:00:23 +0200

    Moved the startup object to the corect namespace.

--- Pokémon3D/GameCore/Program.cs
+++ Pokémon3D/GameCore/Program.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Pokémon3D.GameCore;
 
-/// <summary>
-/// Visual Studio decides to not have any é in the namespace of the startup object.
-/// So we just put "Pokemon3D" here and have Program.cs be in its own little namespace.
-/// </summary>
-namespace Pokemon3D.GameCore
+namespace Pokémon3D.GameCore
 {
     public static class Program
     {
