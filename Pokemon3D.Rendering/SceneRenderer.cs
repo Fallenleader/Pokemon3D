ce46f3561ea245f35a9c5dfd602dd5bcc3d252be
trommlbomml <peter_friedland@gmx.de>
2015-10-11 11:57:17 +0200

    1) Extracted Renderer of Scene to separate class using an interface.
    2) Billboards need to be set als transparent, otherwise will not work properly.

---
+++ Pokemon3D.Rendering/SceneRenderer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pokemon3D.Rendering
+{
+    internal interface SceneRenderer
+    {
+        void Draw(IList<SceneNode> allNodes, IList<Camera> cameras, Vector3 lightDirection, bool enableShadows);
+
+        void DrawDebugShadowMap(SpriteBatch spriteBatch, Rectangle target);
+    }
+}
