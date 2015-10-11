13c5f52b52e3ac4c0f95575bbbd1663912c4d006
trommlbomml <peter_friedland@gmx.de>
2015-10-11 15:59:45 +0200

    1) Created abstract baseclass for objects accessing the game.
    2) Made simple transition to other screen using existing framework

---
+++ Pokémon3D/GameCore/GameContextObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokémon3D.GameCore
+{
+    abstract class GameContextObject
+    {
+        public GameController Game => GameController.Instance;
+    }
+}
