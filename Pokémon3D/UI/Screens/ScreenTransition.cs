4ece2d76645b90d96b755cf8bc3392b860f4378d
trommlbomml <peter_friedland@gmx.de>
2015-10-11 16:41:49 +0200

    Added interface for defining transitions. All Screens are now changed using a blending transition.

---
+++ Pokémon3D/UI/Screens/ScreenTransition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pokémon3D.UI.Screens
+{
+    interface ScreenTransition
+    {
+        void StartTransition(Texture2D source, Texture2D target);
+
+        bool IsFinished { get; }
+
+        void Update(float elapsedTimeSeconds);
+
+        void Draw();
+    }
+}
